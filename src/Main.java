7fa90cb5eb35e3ec5f24364d35c099b96bf443f4
yaquenj <yaquenjs@gmail.com>
2025-03-14 13:12:36 +0100

    Stworzenie repozytorium, dodanie opisów zadań i początkowych funkcji z podziałem na zadania oraz danych.

---
+++ src/Main.java
@@ -0,0 +1,50 @@
+/*
+
+Zadanie 4 Liczby
+
+3000 liczb z przedzialu [2, 2000]
+20 liczb całkowitych z przedziału [2, 1 000 000 000] (10^10)
+rozdzielone w wierszach spacjami
+
+
+4.1
+Ile liczb z pierwszego wiersza jest dzielnikiem jakiejkolwiek liczby spośród liczb z drugiego wiersza
+
+199, tylko liczba 31 wystepujaca raz nie jest dzielnikiem zadnej z liczb w 2 wierszu
+
+4.2
+Z 1 wiersza podaj liczbe ktora jest 101 liczbą w kolejności licząc od największej po ich uporządkowaniu
+
+wśród liczb 2, 4, 2, 3, 3, 4 drugą w kolejności, licząc od największej, jest liczba 4.
+
+4.3
+Dla każdej z liczb z 2 wiersza rozstrzygnij czy da się ją przedstawić jako iloczyn jedynie liczb z pierwszego wiersza. Przy tym liczba wystąpień danego czynnika w iloczynie nie może być większa niż liczba wystąpień tego czynnika w pierwszym wierszy. Znajdź wszystkie które można tak przedstawić.
+
+10 12 14 15 18 20 21 25 27 28
+
+ */
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        zadanie41();
+        zadanie42();
+        zadanie43();
+        zadanie44();
+    }
+    public static void zadanie41() {
+
+
+
+    }
+    public static void zadanie42() {
+
+    }
+    public static void zadanie43() {
+
+    }
+    public static void zadanie44() {
+
+    }
+}
